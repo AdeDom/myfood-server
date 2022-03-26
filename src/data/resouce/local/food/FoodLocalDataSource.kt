59c48acb028e4a84e65607fe15eda49c72696063
pathiphon <pathiphon.ja@chococrm.com>
2022-03-27 01:35:32 +0700

    Clean code food local database.

--- src/data/resouce/local/food/FoodLocalDataSource.kt
+++ src/data/resouce/local/food/FoodLocalDataSource.kt
@@ -1,16 +1,4 @@
 package com.adedom.myfood.data.resouce.local.food
 
-import com.adedom.myfood.route.models.entities.FoodAllEntity
-import com.adedom.myfood.route.models.entities.FoodEntity
-import com.adedom.myfood.route.models.request.InsertFoodRequest
-
 interface FoodLocalDataSource {
-
-    fun insertFood(insertFoodRequest: InsertFoodRequest, status: String): Int?
-
-    fun getFoodDetail(foodId: Int): FoodEntity?
-
-    fun getFoodByCategoryId(categoryId: Int): List<FoodEntity>
-
-    fun getFoodAll(): List<FoodAllEntity>
 }
