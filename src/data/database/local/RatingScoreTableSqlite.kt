0f50330d7cce61bb77c5d589bfc7cf6d0fe147d1
pathiphon <pathiphon.ja@chococrm.com>
2022-03-29 21:04:24 +0700

    Create table rating score on sqlite.

---
+++ src/data/database/local/RatingScoreTableSqlite.kt
@@ -0,0 +1,17 @@
+package com.adedom.myfood.data.database.local
+
+import org.jetbrains.exposed.sql.Table
+
+object RatingScoreTableSqlite : Table(name = "rating_score") {
+
+    val ratingScoreId = text(name = "rating_score_id")
+    val userId = text(name = "user_id")
+    val foodId = integer(name = "food_id")
+    val ratingScore = integer(name = "rating_score")
+    val isBackup = integer(name = "is_backup")
+    val created = text(name = "created")
+    val updated = text(name = "updated").nullable()
+
+    override val primaryKey: PrimaryKey
+        get() = PrimaryKey(ratingScoreId, name = "PK_RatingScore_ID")
+}
