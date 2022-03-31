d3101c232833ef1a20a54c76f11adc5855863255
pathiphon <pathiphon.ja@chococrm.com>
2022-03-31 22:19:14 +0700

    Create table auth on sqlite.

---
+++ src/data/database/sqlite/AuthTableSqlite.kt
@@ -0,0 +1,16 @@
+package com.adedom.myfood.data.database.sqlite
+
+import org.jetbrains.exposed.sql.Table
+
+object AuthTableSqlite : Table(name = "auth") {
+
+    val authId = text(name = "auth_id")
+    val accessToken = text(name = "access_token")
+    val refreshToken = text(name = "refresh_token")
+    val isBackup = integer(name = "is_backup")
+    val created = text(name = "created")
+    val updated = text(name = "updated").nullable()
+
+    override val primaryKey: PrimaryKey
+        get() = PrimaryKey(authId, name = "PK_Auth_ID")
+}
