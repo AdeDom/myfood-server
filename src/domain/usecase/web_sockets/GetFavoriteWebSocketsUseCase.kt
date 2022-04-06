be1d4736e83ef1455aa94f0bb76713d14070f899
pathiphon <pathiphon.ja@chococrm.com>
2022-04-06 15:02:59 +0700

    Add base response on web sockets.

--- src/domain/usecase/web_sockets/GetFavoriteWebSocketsUseCase.kt
+++ src/domain/usecase/web_sockets/GetFavoriteWebSocketsUseCase.kt
@@ -1,17 +1,21 @@
 package com.adedom.myfood.domain.usecase.web_sockets
 
+import com.adedom.myfood.data.models.base.BaseResponse
 import com.adedom.myfood.data.models.web_sockets.GetFavoriteWebSocketsResponse
 import com.adedom.myfood.data.repositories.food.FoodRepository
+import com.adedom.myfood.utility.constant.ResponseKeyConstant
 
 class GetFavoriteWebSocketsUseCase(
     private val foodRepository: FoodRepository,
 ) {
 
-    suspend operator fun invoke(): List<GetFavoriteWebSocketsResponse> {
+    suspend operator fun invoke(): BaseResponse<List<GetFavoriteWebSocketsResponse>> {
+        val response = BaseResponse<List<GetFavoriteWebSocketsResponse>>()
+
         return when {
             else -> {
                 val getFoodAndCategoryAll = foodRepository.getFoodAndCategoryAll()
-                getFoodAndCategoryAll
+                val getFavoriteWebSocketsResponse = getFoodAndCategoryAll
                     .asSequence()
                     .filter {
                         (it.favorite ?: 0L) > 0
@@ -26,6 +30,10 @@ class GetFavoriteWebSocketsUseCase(
                         )
                     }
                     .toList()
+
+                response.status = ResponseKeyConstant.SUCCESS
+                response.result = getFavoriteWebSocketsResponse
+                response
             }
         }
     }
