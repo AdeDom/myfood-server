b5f33811299a1f56ffb746f2fdc4e3fd9e688f0e
pathiphon <pathiphon.ja@chococrm.com>
2022-04-06 15:18:06 +0700

    Handle web sockets my favorite request and response.

--- src/route/web_sockets/FoodWebSockets.kt
+++ src/route/web_sockets/FoodWebSockets.kt
@@ -1,9 +1,13 @@
 package com.adedom.myfood.route.web_sockets
 
-import com.adedom.myfood.domain.usecase.web_sockets.GetFavoriteWebSocketsUseCase
+import com.adedom.myfood.data.models.request.MyFavoriteRequest
+import com.adedom.myfood.data.repositories.Resource
+import com.adedom.myfood.domain.usecase.favorite.MyFavoriteUseCase
+import com.adedom.myfood.utility.extension.userId
 import io.ktor.http.cio.websocket.*
 import io.ktor.routing.*
 import io.ktor.websocket.*
+import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
 import org.kodein.di.instance
@@ -13,12 +17,8 @@ import java.util.*
 fun Route.foodWebSocketsRoute() {
 
     val favoriteConnections = Collections.synchronizedSet<DefaultWebSocketSession>(LinkedHashSet())
-    webSocket("/ws/food/favorite") {
-        val getFavoriteWebSocketsUseCase by closestDI().instance<GetFavoriteWebSocketsUseCase>()
-
-        val getFavoriteWebSocketsUseCaseList = getFavoriteWebSocketsUseCase()
-        val getFavoriteWebSocketsUseCaseListString = Json.encodeToString(getFavoriteWebSocketsUseCaseList)
-        send(getFavoriteWebSocketsUseCaseListString)
+    webSocket("/ws/food/myFavorite") {
+        val myFavoriteUseCase by closestDI().instance<MyFavoriteUseCase>()
 
         favoriteConnections += this
         try {
@@ -26,8 +26,20 @@ fun Route.foodWebSocketsRoute() {
                 frame as? Frame.Text ?: continue
                 val text = frame.readText()
 
-                favoriteConnections.forEach { session ->
-                    session.send(text)
+                val userId = call.userId
+                val myFavoriteRequest = Json.decodeFromString<MyFavoriteRequest>(text)
+                val resource = myFavoriteUseCase(userId, myFavoriteRequest)
+                when (resource) {
+                    is Resource.Success -> {
+                        val response = Json.encodeToString(resource.data)
+                        favoriteConnections.forEach { session ->
+                            session.send(response)
+                        }
+                    }
+                    is Resource.Error -> {
+                        val response = Json.encodeToString(resource.error)
+                        this.send(response)
+                    }
                 }
             }
         } catch (e: Exception) {
